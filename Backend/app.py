59990eabaa0da2547d20fa544b477b2fa10b2851
NTheo2000 <nikolastheo3@gmail.com>
2025-07-10 12:14:57 +0200

    render adjustments

--- Backend/app.py
+++ Backend/app.py
@@ -153,4 +153,6 @@ def preload_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
